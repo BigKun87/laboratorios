8d5b92d7c36ea40f642dc242905cbc1f729d2595
Ricardo Román <reroman4@gmail.com>
2024-12-21 18:53:05 -0600

    Se eliminan imports innecesarios.

--- src/main/java/com/lp/entity/UserEntity.java
+++ src/main/java/com/lp/entity/UserEntity.java
@@ -15,8 +15,6 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
